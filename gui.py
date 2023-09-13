4d62b0f1437d2ef9fdacada3592216a5ac0b802f
Debiddo <dzheng@terpmail.umd.edu>
2023-09-12 20:22:14 -0400

    Implement edit and complete button Fixed some bugs

--- gui.py
+++ gui.py
@@ -9,16 +9,17 @@ list_box = sg.Listbox(values=functions.get_todos(),
                       enable_events=True,
                       size=(45,10))
 edit_button = sg.Button("Edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+layout = [[label,input_box,add_button],
+          [list_box,edit_button,complete_button],
+          [exit_button]]
 
 window = sg.Window("My To-Do App",
-                   layout=[[label,input_box,add_button],[list_box,edit_button]],
+                   layout=layout,
                    font=("Helvetica", 20))
 while True:
     event, values = window.read()
-    print(1, event)
-    print(2, values)
-    #print(3, values["todos"])
     match event:
         case "Add":
             if values["todo"] != "":
@@ -29,9 +30,7 @@ while True:
         case "Edit":
             if values["todos"] != [] and values["todo"] != "" and values["todo"] != values["todos"]:
                 todo = values["todos"][0]
-                print(4, todo)
                 new_todo = values["todo"].title()
-                print(5, new_todo)
                 todos = functions.get_todos()
                 index = todos.index(todo)
                 todos[index] = new_todo + "\n"
@@ -39,6 +38,19 @@ while True:
                 window["todos"].update(values=todos)
         case "todos":
             window["todo"].update(value=values["todos"][0])
+        case "Complete":
+            todo = values["todo"]
+            todos = functions.get_todos()
+            if todo in todos or todo + "\n" in todos:
+                if todo + "\n" in todos:
+                    index = todos.index(todo+"\n")
+                else:
+                    index = todos.index(todo)
+                todos.pop(index)
+                functions.update_todos(todos)
+                window["todos"].update(values=todos)
+        case "Exit":
+            break
         case sg.WINDOW_CLOSED:
             break
 window.close()

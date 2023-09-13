18fb7d87fa6e76b13d3388aa1330805f9d5257e6
Debiddo <dzheng@terpmail.umd.edu>
2023-09-12 20:42:03 -0400

    Added a Clock

--- gui.py
+++ gui.py
@@ -1,6 +1,9 @@
 import functions
 import PySimpleGUI as sg
+import time
 
+sg.theme("DarkTeal12")
+clock = sg.Text('',key="clock")
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="Enter a todo", key="todo")
 add_button = sg.Button("Add")
@@ -11,7 +14,7 @@ list_box = sg.Listbox(values=functions.get_todos(),
 edit_button = sg.Button("Edit")
 complete_button = sg.Button("Complete")
 exit_button = sg.Button("Exit")
-layout = [[label,input_box,add_button],
+layout = [[clock],[label,input_box,add_button],
           [list_box,edit_button,complete_button],
           [exit_button]]
 
@@ -19,7 +22,8 @@ window = sg.Window("My To-Do App",
                    layout=layout,
                    font=("Helvetica", 20))
 while True:
-    event, values = window.read()
+    event, values = window.read(timeout=10)
+    window["clock"].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             if values["todo"] != "":

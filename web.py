fa130258a7e3b8f8b0df1b22d616fc393bd2b54c
Debiddo <dzheng@terpmail.umd.edu>
2023-09-13 20:38:34 -0400

    Implement "Add new todo"

--- web.py
+++ web.py
@@ -6,6 +6,12 @@ import functions
 # Get the todos
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo+"\n")
+    functions.update_todos(todos)
+
+
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
@@ -13,4 +19,7 @@ st.write("This app is to increase your productivity.")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Enter a todo: ",placeholder="Add a new todo...")
+st.text_input(label="Enter a todo: ",placeholder="Add a new todo...",on_change=add_todo, key="new_todo")
+
+# uncomment to show session state in the web app
+st.session_state

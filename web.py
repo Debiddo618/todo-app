958b9429b718cef74f1491faeb5e38c0272b1737
Debiddo <dzheng@terpmail.umd.edu>
2023-09-13 20:54:25 -0400

    Implement "Complete feature"

--- web.py
+++ web.py
@@ -8,18 +8,23 @@ todos = functions.get_todos()
 
 def add_todo():
     todo = st.session_state["new_todo"]
-    todos.append(todo+"\n")
+    todos.append(todo.title()+"\n")
     functions.update_todos(todos)
-
+    st.session_state["new_todo"] = ""
 
 st.title("My Todo App")
 st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity.")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.update_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
 
 st.text_input(label="Enter a todo: ",placeholder="Add a new todo...",on_change=add_todo, key="new_todo")
 
 # uncomment to show session state in the web app
-st.session_state
+# st.session_state
